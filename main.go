5d109a295669c3997d3c212a0530cdb9996369c0
Erickype <erickype@hotmail.com>
2022-08-12 15:11:20 -0500

    Albums data for test

--- main.go
+++ main.go
@@ -8,6 +8,13 @@ type album struct {
 	Price  float64 `json:"price"`
 }
 
+//Albums data for test
+var albums = []album{
+	{Id: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{Id: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{Id: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
 func main() {
 
 }
