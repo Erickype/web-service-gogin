e6dec1b46ac516f1fd68c2735021a201d47cd231
Erickype <erickype@hotmail.com>
2022-08-12 15:35:54 -0500

    Handler function for getAlbums and route in server

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-//Album struct
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Album struct
 type album struct {
 	Id     string  `json:"id"`
 	Title  string  `json:"title"`
@@ -8,7 +14,7 @@ type album struct {
 	Price  float64 `json:"price"`
 }
 
-//Albums data for test
+// Albums data for test
 var albums = []album{
 	{Id: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{Id: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
@@ -16,5 +22,15 @@ var albums = []album{
 }
 
 func main() {
+	router := gin.Default()
 
+	//Handler route for (GET)/albums
+	router.GET("/albums", getAlbums)
+
+	//server listening
+	router.Run("localhost:8080")
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
 }
