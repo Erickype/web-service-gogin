99ff079ccb2e2006ae18821ce82acb134a5fbb16
Erickype <erickype@hotmail.com>
2022-08-12 15:08:43 -0500

    Main function and album struct

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+//Album struct
+type album struct {
+	Id     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+func main() {
+
+}
